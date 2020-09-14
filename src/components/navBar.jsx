fc39dea4194f4ae7546869d51f08dbf2083acba1
Sadman255 <sadmanowar@gmail.com>
2020-09-14 17:11:29 -0400

    first commit

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
-        Partner App
+        Zakat Finance
       </Link>
       <button
         className="navbar-toggler"
